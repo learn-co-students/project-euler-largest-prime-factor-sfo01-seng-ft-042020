1ea2efdf50c74976ebd6bd50ebaf9d63fe15018c
Don Mallory <dmm333@gmail.com>
2020-08-22 12:05:50 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,24 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+    factors=[]
+    drain = input
+    loop do
+        factor = find_factor(drain)
+
+        if factor && factor != input
+            factors << factor
+            drain = drain / factor
+        else
+            break
+        end
+    end
+    factors.max
+end
+
+def find_factor(num)
+    for i in 2..num
+        if num % i == 0
+            return i
+        end
+    end
+    return nil
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,33 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_reader :number
+
+    def initialize(num)
+        @num = num
+        find_largest_factor
+    end
+
+    def find_largest_factor
+        factors=[]
+        drain = @num
+        loop do
+            factor = find_factor(drain)
+    
+            if factor && factor != @num
+                factors << factor
+                drain = drain / factor
+            else
+                break
+            end
+        end
+        @number = factors.max
+    end
+    
+    def find_factor(num)
+        for i in 2..num
+            if num % i == 0
+                return i
+            end
+        end
+        return nil
+    end
+end
